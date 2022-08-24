1a3d07fc1aeac1c95b25148e6531ecc483bf8875
jklbriggs2 <65055353+jklbriggs2@users.noreply.github.com>
2022-08-24 16:22:16 -0600

    unfinished uncommented signal processing code

---
+++ Run_SignalProcessing.py
@@ -0,0 +1,37 @@
+
+# %% 
+# This code is made to extract different frequencies of the signal and save them as individual csv files.
+# Jennifer Briggs 2022
+
+## ---- Options for you to change -------
+# set to true if you'd like to see and save figures. set to false if you don't need figures
+fig_on = True
+
+# if you want to predefine a savepath. If not, comment out this line by putting at # in front!
+global savepath
+savepath = '/Users/jkbriggs/Documents/GitHub/Functional_and_Structural_Networks/Examples/'
+
+
+
+# %% Import packages
+from SignalProcessing import *
+import pandas as pd
+import easygui
+import numpy as np
+
+
+# %%  Load calcium file
+try: # you can directly add the path to your data here
+    ca = pd.read_csv('/Users/jkbriggs/OneDrive - The University of Colorado Denver/Anschutz/Islet/TempData/Erli_calcium.csv')
+except: #if there is no path, it will ask you to select the folder
+    path = easygui.fileopenbox('Select Time signal file')
+    ca = pd.read_csv(path)
+
+try: #if time is in the first axis, we save it and remove
+    time = ca.Time
+    ca = ca.drop('Time', axis=1)
+    fs = 
+except:
+    print('No time avaliable')
+    fs = input('What is the frequency of recording?')
+# %%
