528f304c4fa46470fe8e67d039e65a334b730060
jenniferkbriggs <65055353+jklbriggs2@users.noreply.github.com>
2022-09-08 11:32:15 -0600

    Image processing

---
+++ Extracting_Individual_Cells.m
@@ -0,0 +1,209 @@
+%% THIS PROGRAM IMPORTS CALCIUM IMAGING FILES FOR CELL-BY-CELL ANALYSIS OF CALCIUM TRACE TO IDENTIFY Cell Network
+%% REAL-TIME USER INPUT REQUIRED
+%% Jennifer Briggs, Feb 2021
+%% HOUSEKEEPING
+close all
+clear all
+clc
+
+
+cachannel = 1;
+nuchannel = 2;
+howmanychannel = 1;
+
+filename = '/Volumes/Briggs_10TB/SizeDependence/Analysis/Images/VidInfo.mat' %file name can direct to '.mat' analysis file or imaging file
+savepath = '/Users/brigjenn/OneDrive - The University of Colorado Denver/Anschutz/Islet/STDanalysis/'
+savename = '/PlaywithSTD.mat'
+
+
+
+numad = 1;
+starttime = -1
+endtime = -1
+gstart = 1
+
+    
+%% LOADING THE CA IMAGE FILE
+if exist('R') == 0
+try
+    load([filename], 'R', 'zz', 'zstacks', 'cachannel', 'howmanychannel')
+catch
+    R = bfopen([filename]); % Uses bfopen program to open .czi/.lsm image files
+    try
+        save([savepath savename],  '-V7.3')
+    catch
+        mkdir([savepath])
+        save([savepath savename],  '-V7.3')
+    end
+ end
+end
+
+
+%% 
+% omeMeta = 0
+{1,4};
+% voxelSizeX = omeMeta.getPixelsPhysicalSizeX(0).value(ome.units.UNITS.MICROMETER); % in µm
+% voxelSizeXdouble = voxelSizeX.doubleValue();                                  % The numeric value represented by this object after conversion to type double                             
+tic
+pics=R{1};
+pics=pics(:,1);
+
+for i=1:length(pics)
+    IMG(:,:,i)=pics{i};
+end
+pn = length(pics);
+
+for i=1:pn
+    IMG(:,:,i)=pics{i};
+end
+
+try
+    for i=1:length(pics)
+        T(i)=R{4}.getPlaneDeltaT(0, i-1).value;
+    end
+catch
+    T=0:0.5:pn*0.5;
+end
+T = double(T);
+T = T(cachannel:howmanychannel:end);
+
+
+if starttime(ff) == -1
+    st = 1;
+else
+    st = starttime(ff);
+end
+
+if endtime(ff) == -1
+    ed=length(T);
+else
+    ed=endtime(ff);
+end
+
+T = T(st:ed);
+
+images=double(IMG); % converts images to double precision
+images = images(:,:,cachannel:howmanychannel:end);
+
+RawImg=images(:,:,1); % assigns the first frame of the video to RawImg variable
+
+clear pics R IMG;
+% clear omeMeta;
+
+output_dir = savepath;
+toc
+
+%% DECLARING IMAGE PROPERTIES
+tic
+images = images(:,:,st:ed-1);
+sx=size(images,1);
+sy=size(images,2);
+sz=length(T)-1;
+for i=1:sz
+    images(:,:,i)=medfilt2(images(:,:,i),[5 5]); %applies filter to clean up images
+end
+
+%Makes all frames 1fps
+% fps =Vidinfo(g).fps(ff);
+fps = 1;
+ ct =1
+for i = 1:fps:sz
+    images(:,:,ct) = images(:,:,i);
+    ct = ct+1;
+end
+images = images(:,:,1:ct-1);
+%ImAv = sum(images,3); %compresses all frames into single array of intensities
+ImAv = mean(images,3); 
+HSV = ones(sx,sy,3); %preallocates a 3 dimensional array
+ImAvn = ImAv/max(ImAv(:));
+HSV(:,:,3) = ImAvn.^0.8; %evens out intensity across images
+HSV(:,:,1) = 0.3333;%converts image to green image
+
+RGB2 = hsv2rgb(HSV); %converts to rgb image
+
+RGB= im2gray(RGB2);
+RGB = imadjust(RGB);
+OGFig = figure(1);
+imshow(RGB)
+toc
+%% 
+
+%%
+%% MASKING ISLET FOR CELLS
+
+
+
+%% User Draws ROIs around each cell within the islet
+% ROIs are saved in "CellMask" array and called back to throughout analysis
+CellMask = double(zeros(sx,sy));
+numcells = 1;
+
+% 
+try %try to load in cell masks if already in loadpath
+    try
+    load([loadpath '\Masksv2.mat'])
+    catch
+    load([loadpath '\Masks.mat'])
+    end
+    load([loadpath '\CellNumber.mat'])
+    
+    ImAv = sum(images.*logical(CellMask),3); %compresses all frames into single array of intensities
+    HSV = ones(sx,sy,3); %preallocates a 3 dimensional array
+    ImAvn = ImAv/max(ImAv(:));
+    HSV(:,:,3) = ImAvn.^0.8; %evens out intensity across images
+    HSV(:,:,1) = 0.3333;%converts image to green image
+    RGB2 = hsv2rgb(HSV); %converts to rgb image
+    imshow(RGB2)   
+
+catch
+    NoSigFig = figure('Name','Draw ROIs Around Cells of Interest');
+    imshow(RGB);
+    keyboard
+    k = 1;
+    while k > 0
+        disp('Draw ROIs Around Cells of Interest')
+        ROIMask = imfreehand(); %User draws region around cell
+        ROIMask = createMask(ROIMask); %Mask is created from drawn region
+        CellMask = CellMask + ROIMask.*numcells; %CellMask array is updated with new mask; new mask is multiplied by the cell label before updating
+        CellMask(find(CellMask>numcells)) = numcells; %If a region is overlapped, it is instead attributed to the most recent region
+        UInp = input('Select Additonal ROI? 1=Yes, 0=No, 2=Redraw \n'); % No preallocated number of ROIs, User input required to continue ROI drawing or to stop
+        if UInp == 1 %User input to determine if another region is drawn
+            k = k+1;
+            numcells = numcells+1;
+        elseif UInp == 2
+            numcells = numcells;
+            k = k;
+        elseif UInp ~= 1
+            k = 0;
+        end
+    end
+    save([savepath '\Masks.mat'],'CellMask')
+    save([savepath '\CellNumber.mat'],'numcells')
+    close(NoSigFig);
+    clear NoSigFig;  
+
+
+    %run STD analysis and extract calcium wave form
+    CellTC = STDanalysis(images, CellMask);
+
+    
+    % Plotting traces for entire time course
+    TCFig = figure('Name','Average Intensity Over Time');
+    plot(CellTC);
+    legend(PlotLabels);
+    title(F(ff).name)
+    %clear images MaskedIMGstack;
+    try
+    saveas(TCFig,[savepath '\Cellintestiy.tif']); %Saves figure of each cell's timecourse
+    save([savepath '\CaWaveForm.mat'],'CellTC')
+    catch
+        mkdir(savepath)
+        saveas(TCFig,[savepath '\Cellintestiy.tif']); %Saves figure of each cell's timecourse
+    end
+    save([savepath '\CaWaveForm.mat'],'CellTC')
+    numad = 1+ numad
+    clearvars -except Vidinfo starttime endtime zstacks howmanychannel nuchannel cachannel savename gstart ending filepath ff F imagepaths folderpaths numad AdjacencyAll l g zz zstacks cachannel
+   
+end
+
+
---
+++ STDanalysis.m
@@ -0,0 +1,84 @@
+function CellTC = STDanalysis(images, CellMask,opts)
+%Jennifer Briggs 02.2022
+%This function takes a time course of pixels with masks differentiating
+%cells and removes pixels within that mask that do not correlate with the
+%rest of the pixels (e.g. likely not a part of the cell.)
+
+%images is the image file in matrix form (x,y,t). Currently only accepting
+%3D matrices (e.g. no z stacks) so z stacks should be fed in separately. 
+
+%opts is a structure of options:
+%opts.figs = 1 if want a gif to see the pixels be removed
+
+    CellMasksave = CellMask; %save old cell mask
+    
+    numcells = max(max(CellMask))
+    for i = 1:numcells
+        cctt = 1;
+        badpix = [2 2]; %arbitrary array that will be filled with the bad pixel locations
+        disp(i)
+        while ~isempty(badpix)
+        TCMask = CellMask; %Pulls in CellMask array
+        TCMask(TCMask ~= i) = 0; %Gets rid of all masks besides current one
+        MaskedIMGstack = images.*logical(TCMask); %Applies current mask to all frames of timecourse
+        
+        %get rid of for loop
+        [x,y] = find(TCMask);
+         %Middleimage = MaskedIMGstack(round(mean(x))-5:round(mean(x))+5, round(mean(y))-5:round(mean(y))+5, :);
+        [rr, cc] = find(mean(MaskedIMGstack,3));
+        for ii = 1:size(MaskedIMGstack,3)
+            TCnoZero = MaskedIMGstack(:,:, ii); %Pulls in current frame
+            TCnoZero = TCnoZero(TCnoZero>0); %Accounts for any zeros from preallocation
+            TCcheck(ii,:) = TCnoZero;
+            %TCmiddle(ii) = mean2(Middleimage(:,:,ii));
+            TC(ii) = mean(TCnoZero);
+        end
+        
+        TCchecksm = smoothdata(TCcheck); %Smooth data
+        if size(TCchecksm,2) >0
+        for c=1:size(TCchecksm,2)
+                [C1] = xcorr(TCchecksm(:,c),median(TCchecksm'),3);
+                Maxcor(c) =  max(C1);       
+        end
+        %pixel is bad if it shows no oscillations
+        
+        %badpix = find(Maxcor<5e6);
+        %if length(badpix) > .75*size(TCcheck,2)       
+               badpix = find(Maxcor<mean(Maxcor) - 1.5*std(Maxcor))
+               cctt = cctt +1;
+       %end
+       if cctt < 8  
+
+        figure, title(['Itteration' num2str(cctt)]), plot(rr,cc,'o'), saveas(gcf, ['Itteration' num2str(cctt) '.png']) 
+        for bb= 1:length(badpix)
+        hold on, plot(rr(badpix(bb)),cc(badpix(bb)),'ro','markerfacecolor','r')
+        CellMask(rr(badpix(bb)),cc(badpix(bb))) = 0;
+        end
+       else
+    %        disp('Bad Pixels are too many')
+    %        disp(savepath)
+    %        fig = figure, plot(TCchecksm)
+    %        x = input('Keep deleting or move on? 1 for move on, 0 for keep deleting')
+    %        close(fig)
+    %         if x == 0
+    %             cctt = 1;
+    %         else
+    %             cctt = 5;
+                badpix = [];
+    %         end
+       end
+            clear TCcheck Maxcor Maxcor2
+
+            %figure, plot(TCchecksm)
+         else
+           disp('No pixels')
+           badpix = [];
+           clear TCcheck
+        end
+        end
+
+
+        CellTC(:,i) = TC; %Updates CellTC array with mean intensities from each frame of each cell
+        PlotLabels{i} = ['Cell' num2str(i)]; %Updates labels with current cell number for legend
+    end
+end
